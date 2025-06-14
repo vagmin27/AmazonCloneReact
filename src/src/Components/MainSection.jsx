8483d79f98e6cf5633536b9e8b24f66cc1eafb0d
vagmin27 <vagmin2727@gmail.com>
2025-06-14 22:03:15 +0530

    Add files via upload

---
+++ src/src/Components/MainSection.jsx
@@ -0,0 +1,46 @@
+import { AmazonPay, StylesForMen, Upcominglaunches } from '../Details/OfferDetail';
+import PrimeSlider from "./PrimeSlider";
+import { ProductCard, ProductCard2 } from "./ProductCard";
+import ProductSlider from "./ProductSlider";
+
+const MainSection = () => {
+    return (
+        <>
+        <div id="container">
+            <PrimeSlider/>
+            <div className="product-gallery">
+            <ProductCard
+            title="Up to 70% off | Clearance store"
+            img="../image/Productimage/b1.jpg"
+            offer="Shop Now"
+            />
+            <ProductCard2 Detail={Upcominglaunches}  title="Latest and upcoming launches" />
+            <ProductCard
+            title="Amazon pay Book Travel Tickets"
+            img="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wearables/PC_CategoryCard_379X304_1._SY304_CB614835787_.jpg"
+            offer="Shop Now"
+            />
+            <ProductCard2 Detail={AmazonPay}  title="Amazon pay Book Travel Tickets" />
+            </div>
+            <div className="product-gallery">
+            <ProductCard
+            title="Bluetooth Calling Smartwatch starts at ₹1,999"
+            img="../image/Productimage/b2.jpg"
+            offer="Shop Now"
+            />
+            <ProductCard2 Detail={StylesForMen}  title="Up to 60% off | Styles for men" />
+            <ProductCard
+            title="Amazon pay Book Travel Tickets"
+            img="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wearables/PC_CategoryCard_379X304_1._SY304_CB614835787_.jpg"
+            offer="Shop Now"
+            />
+            <ProductCard2 Detail={AmazonPay}  title="Amazon pay Book Travel Tickets" />
+            </div>
+
+            <ProductSlider />
+        </div>
+        </>
+    );
+};
+
+export default MainSection;
